477156b553ce39f713f0403a64de61054a39d0c1
Keane <bkeaneneoweizhe@gmail.com>
2023-02-25 12:31:56 +0800

    create-claim-page

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,5 +1,72 @@
+import { useState } from "react";
+
 const Create = () => {
-  return <h1>Create</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [receiptNum, setReceiptNum] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [followUpClaim, setFollowUpClaim] = useState(false);
+
+  const handleOnChange = () => {
+    setFollowUpClaim(!followUpClaim);
+  };
+
+  const formatNumber = (num) => {
+    num = num.formatNumber(num, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  };
+  return (
+    <div className="App">
+      <label>First Name:</label>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <br />
+      <label>Last Name:</label>
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <br />
+      <label>Receipt Number:</label>
+      <input
+        type="text"
+        value={receiptNum}
+        onChange={(e) => setReceiptNum(e.target.value)}
+      />
+      <br />
+      <label>Date:</label>
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+      <br />
+      <label>Claim Amount:</label>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => formatNumber(e.target.value)}
+      />
+      <br />
+      <label>Follow-Up Claim?</label>
+      <input
+        type="checkbox"
+        value="Yes"
+        checked={followUpClaim}
+        onChange={handleOnChange}
+      />
+      <br />
+
+      <button type="submit" text="Create New Claim" />
+    </div>
+  );
 };
 
 export default Create;
