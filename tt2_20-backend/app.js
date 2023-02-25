1869d6b33edc3f3e62ef7e9958b84bb70a4326f9
xtemplate24 <71815928+xtemplate24@users.noreply.github.com>
2023-02-25 12:07:00 +0800

    Changed connection settings

--- tt2_20-backend/app.js
+++ tt2_20-backend/app.js
@@ -1,13 +1,4 @@
-var conn = require('./db/db_conn.js')
-var dbsetup = require('./db/setup.js')
-
-<<<<<<< HEAD
-=======
-//pool.query("CREATE TABLE test1 (col1 INT);")
-//pool.query("INSERT INTO test1 (col1) VALUES (1);")
-//conn.query("SELECT * FROM test1;", (err, rows) => console.log(rows))
-dbsetup()
->>>>>>> 4792c2c4b9c3dded2358bc9ee9a6a6254652b96a
+var pool = require('./db/db_conn.js')
 
 var createError = require('http-errors');
 var express = require('express');
@@ -44,15 +35,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 
