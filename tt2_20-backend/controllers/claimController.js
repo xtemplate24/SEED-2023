df026482cf526fc2af0024d719b68b137d0c2c8a
ccybqq <105545077+ccybqq@users.noreply.github.com>
2023-02-25 13:33:32 +0800

    Get all claims

--- tt2_20-backend/controllers/claimController.js
+++ tt2_20-backend/controllers/claimController.js
@@ -1,4 +1,11 @@
+var conn = require('../db/db_conn.js')
+
 exports.get_claims = (req, res) => {
+    let claims = []
+    conn.query("SELECT ic.* FROM user u JOIN insurancepolicies ip ON u.employeeid = ip.employeeid JOIN insuranceclaims ic ON ip.insuranceid = ic.insuranceid WHERE u.employeeid = ?",
+        [req.params.employeeId],
+        (err, result) => claims = result)
+    console.log(claims)
     res.send("NOT IMPLEMENTED: get all claim based on insurance id");
 };
 
