75ae6c99beca7e43609e2277bf2857596a2bcfd9
xtemplate24 <71815928+xtemplate24@users.noreply.github.com>
2023-02-25 13:09:27 +0800

    [Done with KYAWTSAN99] Created sign up API

--- tt2_20-backend/routes/users.js
+++ tt2_20-backend/routes/users.js
@@ -4,28 +4,44 @@ const conn = require("../db/db_conn");
 
 /* GET users listing. */
 
-router.get("/create", function (req, res, next) {
-  res.send("respond with a resource");
+//CREATE API
+router.post("/signup", (req, res) => {
+  let username = req.query.username;
+  let password = req.query.password;
+  let firstname = req.query.firstname;
+  let lastname = req.query.lastname;
+  let age = req.query.age;
+
+  const values = [username, password, firstname, lastname, age];
+
+  conn.query(
+    "SELECT * FROM user WHERE EmployeeID = ?",
+    [username],
+    function (err, data) {
+      console.log(data)
+      if (data.length == 0) {
+        conn.query(
+          "INSERT INTO user (EmployeeID, Password, FirstName, LastName, Age) VALUES(?)",
+          [values],
+          function (err, data) {
+            res.status(201).json({
+              status: "user created",
+            });
+          }
+        );
+      } else {
+        res.status(401).json({
+          status: "username exists",
+        });
+      }
+    }
+  );
 });
 
-router.get('/create', function(req, res, next) {
-  res.send('respond with a resource');
-
-
-  
-});
-
-//Editted login page inputs
+//Login API
 router.post("/login", (req, res) => {
   let username = req.query.username;
   let password = req.query.password;
-  // let password = req.body.password;
-  // if (username === 'admin' && password === 'admin') {
-  //     res.send('Login successful');
-  // }
-  // else {
-  //     res.send('Login failed');
-  // }
   console.log(username, password);
   conn.query(
     "SELECT * FROM user WHERE EmployeeID = ?",
@@ -44,7 +60,6 @@ router.post("/login", (req, res) => {
       } else {
         res.status(401).json({
           status: "incorrect username or password",
-
         });
       }
     }
