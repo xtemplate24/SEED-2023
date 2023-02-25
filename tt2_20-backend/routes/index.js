c91d6e1dd451da78ba0c51acc3a93d0e77a0b632
kyawtsan99 <42372441+kyawtsan99@users.noreply.github.com>
2023-02-25 12:16:36 +0800

    Added post method in user.js

--- tt2_20-backend/routes/index.js
+++ tt2_20-backend/routes/index.js
@@ -1,25 +1,9 @@
 var express = require('express');
-const conn = require("../db/db_conn");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  
-
-  conn.query('SELECT * FROM insuranceclaims', function (err,data,field){
-    if(err) return next(new AppError(err))
-    res.status(200).json({
-     status: "success",
-     length: data?.length,
-     data: data,
-})
-  })
-
-  console.log('hiii')
-
-  });
-
-
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
