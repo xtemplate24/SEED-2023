a1f9a5c0e74a35e5bb486f50c779e70a2afb5b11
kyawtsan99 <42372441+kyawtsan99@users.noreply.github.com>
2023-02-25 12:13:15 +0800

    Add post method in users.js

--- tt2_20-backend/routes/users.js
+++ tt2_20-backend/routes/users.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
+
+//Editted to get the login page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+//Editted login page inputs
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  if (username === 'admin' && password === 'admin') {
+      res.send('Login successful');
+  }
+  else {
+      res.send('Login failed');
+  }
+})
+
 module.exports = router;
