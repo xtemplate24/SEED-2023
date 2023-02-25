6b93ed245e34f76fcfc899bdaa86b451118185e7
RINA <rinaleern98@gmail.com>
2023-02-25 13:06:07 +0800

    added forget password

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -8,6 +8,11 @@ import {
   OutlinedInput,
   InputAdornment,
   IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
@@ -20,7 +25,15 @@ const Login = () => {
       password: "",
     },
   });
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const Login = (data) => {
     console.log(data);
   };
@@ -107,6 +120,52 @@ const Login = () => {
           Login
         </Button>
       </form>
+      <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+          Forget Password?
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Forgot your password?</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the email you use to sign in and we will send you an email
+              with the recovery instructions.
+            </DialogContentText>
+
+            <Controller
+              name="username"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="name"
+                  label="Email Address"
+                  type="email"
+                  fullWidth
+                  variant="standard"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  helperText={error ? error.message : null}
+                  required={true}
+                />
+              )}
+              rules={{
+                required: "Username is required",
+              }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Send Email</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     </Box>
   );
 };
